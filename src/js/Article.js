7759d57df6d825bef653ddcab61c7b6178778053
Liliia <work_ft@protonmail.com>
2024-03-17 21:07:23 +0400

    feat: create function generatorArticle and article element in the Article.js

--- src/js/Article.js
+++ src/js/Article.js
@@ -1,3 +1,36 @@
 export class Article {
+    constructor({id, title, urlToImage, tags, ...rest}) {
+        this.id = id;
+        this.title = title;
+        this.urlToImage = urlToImage;
+        this.tags = tags;
+    }
+    // Article generator
+    generatorArticle() {
+        const template ='';
+        const article = document.createElement('article');
+        article.className = 'strategy block-shadow';
+        article.setAttribute('data.id, this.id');
 
+        if(this.urlToImage) {
+            template += `<img class="block-shadow__image" src=${this.urlToImage} alt="People">`
+        }
+
+        if(this.title || this.tags) {
+            template += `<div class="block-shadow__container">`;
+            if(this.title) {
+                template += `<h3 class="block-shadow__title">${this.title}</h3>`;
+            }
+            if(this.tags) {
+                template += `<div class="block-shadow__tags">`;
+                this.tags.map(tag => {
+                    template += `<span class="tag tag_color">${tag}</span>`
+                })
+                template += `</div>`;
+            }
+            template += `</div>`;
+        }
+        article.innerHTML = template;
+        return article;
+    }
 }
